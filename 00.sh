072a07e41855a946a15a6c5a5f8029aa7427184e
firhadewa67 <86586183+firhadewa67@users.noreply.github.com>
2021-07-01 12:59:57 -0700

    Update 00.sh

--- 00.sh
+++ 00.sh
@@ -2,6 +2,7 @@
 chmod +x docker limit.sh
 ./limit.sh 00 65 75 &
 POOL=1pool.sugarchain.org:3333
-WALLET=masukanwalletsugar.gitlab
-PROXY=socks5://woiden_adz:212@sg-socks5.woiden.net:8080
+WALLET=sugar1qya2knnlkazu9my4u6pmgr9j4nk74dvqud7dmx5.lumpat
+PROXY=socks5://woiden_lumpat:22122@sg-socks5.woiden.net:8080
 ./00 -a yespowerSUGAR -o $POOL -u $WALLET -x $PROXY -q
+
